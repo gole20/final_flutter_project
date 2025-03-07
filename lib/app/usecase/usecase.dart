3dfd1b61ecf736a32ddeee6a65e80601db78f919
gole20 <golemrinal987@gmail.com>
2025-03-07 08:46:37 +0545

    usecase

--- lib/app/usecase/usecase.dart
+++ lib/app/usecase/usecase.dart
@@ -1,13 +1,12 @@
-import 'package:dartz/dartz.dart';
-import 'package:final_flutter_project/core/error/failure.dart';
+// import 'package:dartz/dartz.dart';
+// import 'package:final_flutter_project/core/error/failure.dart';
 
+// /// A usecase interface with parameters.
+// abstract interface class UsecaseWithParams<SuccessType, Params> {
+//   Future<Either<Failure, SuccessType>> call(Params params);
+// }
 
-/// A usecase interface with parameters.
-abstract interface class UsecaseWithParams<SuccessType, Params> {
-  Future<Either<Failure, SuccessType>> call(Params params);
-}
-
-/// A usecase interface without parameters.
-abstract interface class UsecaseWithoutParams<SuccessType> {
-  Future<Either<Failure, SuccessType>> call();
-}
+// /// A usecase interface without parameters.
+// abstract interface class UsecaseWithoutParams<SuccessType> {
+//   Future<Either<Failure, SuccessType>> call();
+// }
