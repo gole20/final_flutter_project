341c1776b0168dfc513416e753f1cdb9abf14d03
gole20 <golemrinal987@gmail.com>
2025-03-07 08:46:28 +0545

    app.dart

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,5 @@
-import 'package:final_flutter_project/features/shoe/data/model/shoe_hive_model.dart';
-import 'package:final_flutter_project/home/home_page_view.dart';
+import 'package:final_flutter_project/features/auth/data/model/shoe_hive_model.dart';
+import 'package:final_flutter_project/splash/shoe_splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
@@ -10,7 +10,7 @@ void main() async {
   // Register the adapter
   Hive.registerAdapter(ShoeHiveModelAdapter());
 
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -19,9 +19,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Kickback Shoes',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const HomePage(),
+      home: const ShoeSplashView(),
     );
   }
 }
