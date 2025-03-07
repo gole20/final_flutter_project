1f15aa5fcb7fb046f9579efb2be74b1beadb9a29
gole20 <golemrinal987@gmail.com>
2025-03-07 08:57:02 +0545

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-import 'package:final_flutter_project/app/app.dart';
 import 'package:final_flutter_project/features/auth/data/model/shoe_hive_model.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+
+import 'app/app.dart';
 
 // final
 // const
